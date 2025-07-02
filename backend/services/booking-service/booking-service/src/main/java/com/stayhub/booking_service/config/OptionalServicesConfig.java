25f30f09581a6d52d4f2f94e7de44ce80a259745
Strahil Peykov <strahil.peykov@gmail.com>
2025-07-02 14:55:24 +0200

    move

--- backend/services/booking-service/booking-service/src/main/java/com/stayhub/booking_service/config/OptionalServicesConfig.java
+++ backend/services/booking-service/booking-service/src/main/java/com/stayhub/booking_service/config/OptionalServicesConfig.java
@@ -1,4 +1,4 @@
-package com.stayhub.service.config;
+package com.stayhub.booking_service.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
