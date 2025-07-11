791aca6d164b7498ba88c6e14c1e857e638d12d7
Strahil Peykov <strahil.peykov@gmail.com>
2025-07-11 09:21:05 +0200

    pom.xml admin

--- backend/services/admin-server/src/main/java/com/stayhub/admin_server/AdminServerApplication.java
+++ backend/services/admin-server/src/main/java/com/stayhub/admin_server/AdminServerApplication.java
@@ -3,7 +3,6 @@ package com.stayhub.admin_server;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
--- backend/services/admin-server/src/main/java/com/stayhub/admin_server/config/AdminUIConfig.java
+++ backend/services/admin-server/src/main/java/com/stayhub/admin_server/config/AdminUIConfig.java
@@ -1,6 +1,5 @@
 package com.stayhub.admin_server.config;
 
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.web.client.HttpHeadersProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,14 +15,5 @@ public class AdminUIConfig {
             headers.set("X-Custom-Header", "StayHub-Admin");
             return headers;
         };
-    }
-    
-    @Bean
-    public de.codecentric.boot.admin.server.ui.config.AdminServerUiProperties.UiRoute customUiRoute() {
-        return new de.codecentric.boot.admin.server.ui.config.AdminServerUiProperties.UiRoute(
-            "custom",
-            "/custom/**",
-            "classpath:/META-INF/spring-boot-admin-server-ui/custom/"
-        );
     }
 }
